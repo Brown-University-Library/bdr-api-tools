ff0666e9351d52deeb1c6ff58fb05208ce55f50d
Birkin James Diana <birkin.diana@gmail.com>
2025-11-09 21:08:48 -0500

    adds initial doctring.

--- list_named_entities.py
+++ list_named_entities.py
@@ -6,4 +6,29 @@
 # ]
 # ///
 
+"""
+Runs spaCy's named entity recognition on an item's extracted-text, if it exists.
 
+Usage:
+  uv run ./list_named_entities.py --item-pid bdr:bfttpwkj
+
+Args:
+  --item-pid (required)
+"""
+
+BASE = 'https://repository.library.brown.edu'
+ITEM_URL_TPL = f'{BASE}/api/items/{{pid}}/'
+STORAGE_URL_TPL = f'{BASE}/storage/{{pid}}/EXTRACTED_TEXT/'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    """
+    Runs main() and exits with its return value.
+    - If all goes well, returns 0.
+    - Otherwise, returns the value returned by main().
+    """
+    raise SystemExit(main())
